0d70cf9cf16bcb48fea7ceed46a7290550f482c0
Vincent Roy <VincentRoy8@gmail.com>
2008-07-18 19:11:49 -0300

    just calls messages and set the html of the div in the iframe

--- public/script.js
+++ public/script.js
@@ -1,18 +1,13 @@
 $(document).ready(function() {
   
   setInterval(function() {
-    var initial = $('#messages').val();
     
-    $.get("messages", { number: 2 }, function(data){
-      if (initial != data) {
-        $('#messages').val(data).css('color', 'red');
-        
-        setTimeout(function() {
-          $('#messages').css('color', 'black');
-        }, 500);
-      }
+    $.get("messages", {}, function(data) {
+      $("#messages").html(data);
     });
-  }, 3000);
+    
+  }, 10000);
   
 });
+
 
